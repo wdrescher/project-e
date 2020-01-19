422b721b6c03e53f39feffbdcd727b90abf7dfd6
Andy Fleming <afleming26@tamu.edu>
2020-01-19 13:06:21 -0600

    added sale date to Jobs Table

--- project_e/jobs/models.py
+++ project_e/jobs/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from project_e.customers.models import Customer
 from project_e.dealers.models import Dealer
 from project_e.contractors.models import Contractor
+from datetime import datetime
 
 
 
@@ -14,7 +15,7 @@ class Job(models.Model):
     dealer_id = models.ForeignKey(Dealer, blank=True, null=True, on_delete=models.CASCADE)
     #salesman_id = models.IntegerField()
     cont_id = models.ForeignKey(Contractor, blank=True, null=True, on_delete=models.CASCADE)
-    sale_date = models.DateTimeField(_("Date of Sale"), blank=True, null=True)
+    sale_date = models.DateTimeField(_("Date of Sale"), default=datetime.now, blank=True)
     contractor_notified_date = models.DateTimeField(_("Date Given To Contractor"), blank=True, null=True)
     contractor_completed_date = models.DateTimeField(_("Date Completed by Contractor"), blank=True, null=True)
     pref_inst_day1 = models.DateTimeField(_("Preferred Installation Time 1"), blank=True, null=True)
